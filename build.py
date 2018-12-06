724b431b0ff2569bb6a81f4f06841bcdabdcb82f
Uilian Ries <uilianries@gmail.com>
2018-12-06 16:12:50 -0200

    #118 Add upload repo
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     reference = conan_packager.reference.__repr__()
     recipe_data = conan_packager.conan_api.inspect(".", ["alias"])
     conan_packager.conan_api.export_alias(reference, recipe_data["alias"])
+    conan_packager.run()
     conan_packager.uploader.upload_packages(reference, upload=conan_packager._upload_enabled(), package_id=None)
